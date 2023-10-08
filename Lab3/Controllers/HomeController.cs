ac5d94e0451c57cb2a3ac5a3db769da41628e9e6
Xylo <cezary2000@gmail.com>
2023-10-08 14:37:28 +0200

    Completed Lab3

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly PhoneBookService _phoneBook;
+
+        public HomeController(ILogger<HomeController> logger, PhoneBookService phoneBook)
+        {
+            _logger = logger;
+            _phoneBook = phoneBook;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_phoneBook.GetContacts());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _phoneBook.Add(contact);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (!_phoneBook.IsIdValid(id))
+            {
+                return NotFound();
+            }
+            
+            _phoneBook.Remove(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
