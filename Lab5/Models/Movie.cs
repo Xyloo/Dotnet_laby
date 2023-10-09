80c733bccfa1b57646a08093c919c9e72cf3688e
Xylo <s95408@pollub.edu.pl>
2023-10-09 20:06:37 +0200

    Lab5 incomplete, finished 5.4

---
+++ Lab5/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab5.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set;}
+        public string TrailerLink { get; set; }
+    }
+}
