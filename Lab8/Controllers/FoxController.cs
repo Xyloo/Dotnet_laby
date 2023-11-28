d924e2d5eee8c6d862467d81a9186e9284a0b8d0
Xylo <s95408@pollub.edu.pl>
2023-11-28 23:04:13 +0100

    Lab8 almost done - client app is missing (should be provided but wasn't)

---
+++ Lab8/Controllers/FoxController.cs
@@ -0,0 +1,67 @@
+﻿using Lab8.Data;
+using Lab8.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab8.Controllers
+{
+    [Route("api/fox")]
+    [ApiController]
+    public class FoxController : ControllerBase
+    {
+        private readonly IFoxesRepository _foxesRepository;
+
+        public FoxController(IFoxesRepository foxesRepository)
+        {
+            _foxesRepository = foxesRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_foxesRepository.GetAll().OrderByDescending(f => f.Loves).ThenBy(f => f.Hates));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var fox = _foxesRepository.Get(id);
+            return fox == null ? NotFound() : Ok(fox);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Add([FromBody] Fox f)
+        {
+            _foxesRepository.Add(f);
+            return CreatedAtAction(nameof(Get), new { id = f.Id }, f);
+        }
+
+        [HttpPut("love/{id}")]
+        public IActionResult Love(int id)
+        {
+            var fox = _foxesRepository.Get(id);
+            if (fox == null)
+                return NotFound();
+
+            fox.Loves++;
+            _foxesRepository.Update(id, fox);
+            
+            return Ok(fox);
+        }
+
+        [HttpPut("hate/{id}")]
+        public IActionResult Hate(int id)
+        {
+            var fox = _foxesRepository.Get(id);
+            if (fox == null)
+                return NotFound();
+
+            fox.Hates++;
+            _foxesRepository.Update(id, fox);
+            
+            return Ok(fox);
+        }
+    }
+}
---
+++ Lab8/Data/IFoxesRepository.cs
@@ -0,0 +1,12 @@
+﻿using Lab8.Models;
+
+namespace Lab8.Data
+{
+    public interface IFoxesRepository
+    {
+        void Add(Fox f);
+        Fox? Get(int id);
+        IEnumerable<Fox> GetAll();
+        void Update(int id, Fox f);
+    }
+}
